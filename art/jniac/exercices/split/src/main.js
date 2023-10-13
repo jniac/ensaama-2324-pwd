52e9625654160c87dbabb5ae1456d54e56d2d55a
jniac <joseph.merdrignac@gmail.com>
2023-10-13 07:42:48 +0200

    clean

--- art/jniac/exercices/split/src/main.js
+++ art/jniac/exercices/split/src/main.js
@@ -2,11 +2,11 @@ import { VisibilityTracker } from '../../../../../../common-resources/js/Visibil
 import { setLeftImageSrc } from './left.js'
 
 for (const img of document.querySelectorAll('.right img')) {
-  const tracker = new  VisibilityTracker(img) 
+  const tracker = new VisibilityTracker(img)
   tracker.onPass({
     threshold: 1,
     above() {
       setLeftImageSrc(img.src)
     },
-  }) 
+  })
 }
