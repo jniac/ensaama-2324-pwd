9f475129802a05da59710b23b9ed1466fc0c3335
Graigouzzz <graig.lowen@ensaama.net>
2023-11-29 11:43:17 +0100

    random color

--- art/Graigouzz/exercice/find_the_number_v1/main.js
+++ art/Graigouzz/exercice/find_the_number_v1/main.js
@@ -1,4 +1,4 @@
-const main= document.querySelector('main')
+const main=document.querySelector('main')
 
 const arrowSource=document.querySelector('.arrow')
 
@@ -11,3 +11,4 @@ main.onclick = (event) => {
     clone.style.top=`${event.clientY}px`
     main.append(clone)
 }
+
