173b92d990fa331f206809486338deb5db0a4ff9
kevinhascoet <kevin.hascoet@ensaama.net>
2023-11-29 11:47:41 +0100

    s

--- art/kevinhascoet/exercices/number2/main.js
+++ art/kevinhascoet/exercices/number2/main.js
@@ -1,1 +1,16 @@
-alert('yo')
+const main = document.querySelector('main')
+const arrowSource = document.querySelector('.arrow')
+
+main.onclick = (event) => {
+    console.log(event.clientY)
+
+    const clone = arrowSource.cloneNode(true)
+    clone.style.top = `${event.clientY}px`
+    main.append(clone)
+}
+
+
+//rdm gradient color
+const color = randFFFFFF()
+const gradient = 'linear-gradient'($!
+)
