fe85efaa2d3eddd54bd0c6d4d9e158ba772c3bd3
paragorika <sofiya.krasilnikova@ensaama.net>
2023-12-13 11:56:48 +0100

    è

--- art/paragorika/exercices/find-the-number-v2/main.js
+++ art/paragorika/exercices/find-the-number-v2/main.js
@@ -11,7 +11,7 @@ makeArtIntro()
 const input = document.querySelector('input')
 input.onchange = ()=>{
   clearArt()
-}
+
 
 const userNumber = Number.parseFloat(input.value)
 input.value = ''
@@ -19,5 +19,10 @@ if (Number.isNaN(userNumber)) {
   gameOutput.classList.remove('hidden')
   gameOutput.innerHTML = 'Un nombre stp.'
 } else if (userNumber < hiddenNumber) {
-  
+  gameOutput.classList.remove('hidden')
+  gameOutput.innerHTML = 'Trop petit'
+} else if (userNumber > hiddenNumber) {
+  gameOutput.classList.remove('hidden')
+  gameOutput.innerHTML = 'Trop grand' 
+}
 }
