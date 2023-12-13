8d01682ebfa6d7696e41419ef2f462a8d7e6521a
paragorika <sofiya.krasilnikova@ensaama.net>
2023-12-13 11:02:09 +0100

    tf

--- art/paragorika/exercices/find-the-number-v2/main.js
+++ art/paragorika/exercices/find-the-number-v2/main.js
@@ -1,18 +1,15 @@
-
 import { lerpFFFFFF, randFFFFFF } from '../../../../common-resources/js/color-utils.js'
-
-
 
 const main = document.querySelector('main')
 const arrowUpSource = document.querySelector('.arrow.up')
 const arrowDownSource = document.querySelector('.arrow.down')
 
+// Les "sources" ne sont plus nécessaires, on peut les retirer de l'écran:
 arrowUpSource.remove()
 arrowDownSource.remove()
 
-
-const colorA = '#ff8080'
-const colorB = '#2e2edd'
+const colorA = '#CFEED0'
+const colorB = '#C2A8F1'
 
 const randomColorAorB = () => {
   const colors = [colorA, colorB]
@@ -24,34 +21,37 @@ const randomLerpColor = () => {
   return lerpFFFFFF(colorA, colorB, Math.random())
 }
 
+
+
 function addArrowUp(y) {
-    const clone = arrowUpSource.cloneNode(true)
-    clone.style.top = `${100 - y}%`
-    main.append(clone)
+  const clone = arrowUpSource.cloneNode(true)
+  clone.style.top = `${100 - y}%`
+  main.append(clone)
 
-    const color = randFFFFFF()
-const linearGradient = `linear-gradient(${color}, ${color}00)`
-clone.querySelector('.right').style.backgroundImage = linearGradient
-clone.querySelector('.left').style.backgroundImage = linearGradient
+  // random gradient color
+  const color = randomLerpColor()
+  const linearGradient = `linear-gradient(${color}, ${color}00)`
+  clone.querySelector('.right').style.backgroundImage = linearGradient
+  clone.querySelector('.left').style.backgroundImage = linearGradient
 }
-
-
 
 function addArrowDown(y) {
-    const clone = arrowDownSource.cloneNode(true)
-    clone.style.bottom = `${100 - y}%`
-    main.append(clone)
-    
-    const color = randFFFFFF()
-const linearGradient = `linear-gradient(${color}00, ${color})`
-clone.querySelector('.right').style.backgroundImage = linearGradient
-clone.querySelector('.left').style.backgroundImage = linearGradient
+  const clone = arrowDownSource.cloneNode(true)
+  clone.style.bottom = `${100 - y}%`
+  main.append(clone)
+
+  // random gradient color
+  const color = randomLerpColor()
+  const linearGradient = `linear-gradient(${color}, ${color}00)`
+  clone.querySelector('.right').style.backgroundImage = linearGradient
+  clone.querySelector('.left').style.backgroundImage = linearGradient
 }
+
 
 function makeArt(){
     for (let i = 0; i < 20; i++) {
-        addArrowDown(i * 8)
-        addArrowUp(i * 8)
+        addArrowDown(i * 9)
+        addArrowUp(i * 9)
     }
     
 }
--- art/pierrenoi/exercices/find-the-number-v1/main.js
+++ art/pierrenoi/exercices/find-the-number-v1/main.js
@@ -1,35 +1,50 @@
-import { randFFFFFF } from '../../../../common-resources/js/color-utils.js'
-
+import { lerpFFFFFF, randFFFFFF } from '../../../../common-resources/js/color-utils.js'
 
 const main = document.querySelector('main')
 const arrowUpSource = document.querySelector('.arrow.up')
 const arrowDownSource = document.querySelector('.arrow.down')
 
+// Les "sources" ne sont plus nécessaires, on peut les retirer de l'écran:
 arrowUpSource.remove()
 arrowDownSource.remove()
 
-function addArrowUp(y) {
-    const clone = arrowUpSource.cloneNode(true)
-    clone.style.top = `${100 - y}%`
-    main.append(clone)
+const colorA = '#ff8080'
+const colorB = '#2e2edd'
 
-    const color = randFFFFFF()
-const linearGradient = `linear-gradient(${color}, ${color}00)`
-clone.querySelector('.right').style.backgroundImage = linearGradient
-clone.querySelector('.left').style.backgroundImage = linearGradient
+const randomColorAorB = () => {
+  const colors = [colorA, colorB]
+  const index = Math.floor(colors.length * Math.random())
+  return colors[index]
+}
+
+const randomLerpColor = () => {
+  return lerpFFFFFF(colorA, colorB, Math.random())
 }
 
 
 
+function addArrowUp(y) {
+  const clone = arrowUpSource.cloneNode(true)
+  clone.style.top = `${100 - y}%`
+  main.append(clone)
+
+  // random gradient color
+  const color = randomLerpColor()
+  const linearGradient = `linear-gradient(${color}, ${color}00)`
+  clone.querySelector('.right').style.backgroundImage = linearGradient
+  clone.querySelector('.left').style.backgroundImage = linearGradient
+}
+
 function addArrowDown(y) {
-    const clone = arrowDownSource.cloneNode(true)
-    clone.style.bottom = `${100 - y}%`
-    main.append(clone)
-    
-    const color = randFFFFFF()
-const linearGradient = `linear-gradient(${color}00, ${color})`
-clone.querySelector('.right').style.backgroundImage = linearGradient
-clone.querySelector('.left').style.backgroundImage = linearGradient
+  const clone = arrowDownSource.cloneNode(true)
+  clone.style.bottom = `${100 - y}%`
+  main.append(clone)
+
+  // random gradient color
+  const color = randomLerpColor()
+  const linearGradient = `linear-gradient(${color}, ${color}00)`
+  clone.querySelector('.right').style.backgroundImage = linearGradient
+  clone.querySelector('.left').style.backgroundImage = linearGradient
 }
 
 for (let i = 0; i < 20; i++) {
