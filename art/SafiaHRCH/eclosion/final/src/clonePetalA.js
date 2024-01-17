818af2808975c93f347d2764d8d2ec7b41422548
SafiaHRCH <safia.harireche@ensaama.net>
2024-01-17 11:46:11 +0100

    change

--- art/SafiaHRCH/eclosion/final/src/clonePetalA.js
+++ art/SafiaHRCH/eclosion/final/src/clonePetalA.js
@@ -1,8 +1,8 @@
 export function clonePetalA() {
   const petal = document.querySelector('.sah .petal-a');
-  for (let i = 1; i < 25; i++) {
+  for (let i = 1; i < 20; i++) {
     const clone = petal.cloneNode(true);
-    clone.style.setProperty('--angle', `${(360 / 25) * i}deg`);
+    clone.style.setProperty('--angle', `${(360 / 20) * i}deg`);
     petal.parentElement.append(clone);
   }
 }
