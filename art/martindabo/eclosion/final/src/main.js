9b0143e86414b864bd858ad3bb61e2df02d7622a
martindabo <martin.d'aboville@ensaama.net>
2024-01-17 11:11:32 +0100

    dfgd

--- art/martindabo/eclosion/final/src/main.js
+++ art/martindabo/eclosion/final/src/main.js
@@ -3,3 +3,10 @@ import { replaceByExternalRef } from '../../../../../common-projects/eclosion/sr
 
 replaceByExternalRef()
 initEclosion('mar')
+
+const petal = document.querySelector('.mar .petal-a')
+for (let i = 1; i < 4; i++) {
+  const clone = petal.cloneNode(true)
+  clone.style.queryProperty('--angle' , `${(360/4) * i}deg`)
+  petal.parentElement.append(clone)
+}
