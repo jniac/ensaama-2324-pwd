349c7cafa4a1e0c2cafc1add7d03f395d836659a
ldssrt <louiselunadessart@gmail.com>
2024-01-17 11:55:14 +0100

    m

--- art/ldssrt/eclosion/final/src/main.js
+++ art/ldssrt/eclosion/final/src/main.js
@@ -5,4 +5,14 @@ import { clonePetalA } from './clonePetalA.js'
 replaceByExternalRef()
 initEclosion('lds')
 
+function clonePetalA() {
+
+  const petal = document.querySelector('lds .petal-a');
+  for (let i = 1; i < 4; i++) {
+    const clone = petal.cloneNode(true);
+    clone.style.setProperty('--angle', `${(360 / 4) * i}deg`);
+    petal.parentElement.append(clone);
+  }
+}
+
 clonePetalA()
