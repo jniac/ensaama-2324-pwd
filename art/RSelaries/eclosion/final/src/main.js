73023f9305a64f856dab801666ae9bff4b7eecc7
RSelaries <raphael.selaries@ensaama.net>
2024-01-24 10:14:30 +0100

    oula j'ai r suivi

--- art/RSelaries/eclosion/final/src/main.js
+++
@@ -1,5 +0,0 @@
-import { initEclosion } from '../../../../../common-projects/eclosion/src/eclosion.js'
-import { replaceByExternalRef } from '../../../../../common-projects/eclosion/src/tools.js'
-
-replaceByExternalRef()
-initEclosion('jnc')
