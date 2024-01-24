c666668dee6ddbd86127c84176e63c95c972bc85
RSelaries <raphael.selaries@ensaama.net>
2024-01-24 11:44:27 +0100

    oulq l4qnie chelou

--- art/RSelaries/eclosion/clonePetalA.js
+++ art/RSelaries/eclosion/clonePetalA.js
@@ -1,8 +1,10 @@
 export function clonePetalA() {
-  const petal = document.querySelector(".rse .petal-a");
-  for (let i = 1; i < 3; i++) {
-      const clone = petal.cloneNode(true);
-      clone.style.setProperty("--angle", ` ${120 * i}deg `);
-      petal.parentElement.append(clone);
-  }
+    const petal = document.querySelector(".rse .petal-1");
+    for (let i = 1; i < 3; i++) {
+        const clone = petal.cloneNode(true);
+        clone.classList.remove('petal-1')
+        clone.classList.add(`petal-${i+1}`)
+        clone.style.setProperty("--angle", ` ${120 * i}deg `);
+        petal.parentElement.append(clone);
+    }
 }
--- art/RSelaries/eclosion/main.js
+++ art/RSelaries/eclosion/main.js
@@ -1,8 +1,17 @@
 import { initEclosion } from "../../../../../common-projects/eclosion/src/eclosion.js";
-import { replaceByExternalRef } from "../../../../../common-projects/eclosion/src/tools.js";
+import { replaceByExternalRef, svgFactory } from "../../../common-projects/eclosion/src/tools.js";
 import { clonePetalA } from './clonePetalA.js'
 
 replaceByExternalRef();
 initEclosion("rse");
+
+
+const d = svgFactory.path.polygon(6, 120, { offsetTurn: 1 / 12 })
+
+const hexagone = svgFactory.createNode(`<path d="${d}"/>`)
+document.querySelector('.core svg').append(hexagone)
+
+const hexagone1 = svgFactory.createNode(`<path d="${d}"/>`)
+document.querySelector('.petals svg').append(hexagone1)
 
 clonePetalA()
