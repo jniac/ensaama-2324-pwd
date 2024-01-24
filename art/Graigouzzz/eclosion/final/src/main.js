187c3263b4b3f6b304e7fa92f9b5732ab4f2572f
Graigouzzz <graig.lowen@ensaama.net>
2024-01-24 12:19:36 +0100

    eclosion

--- art/Graigouzzz/eclosion/final/src/main.js
+++ art/Graigouzzz/eclosion/final/src/main.js
@@ -1,5 +1,11 @@
 import { initEclosion } from '../../../../../common-projects/eclosion/src/eclosion.js'
 import { replaceByExternalRef } from '../../../../../common-projects/eclosion/src/tools.js'
+import { cloneBigPetal, clonePetalA } from './clonePetalA.js'
 
 replaceByExternalRef()
-initEclosion('jnc')
+initEclosion('gra')
+
+
+clonePetalA()
+cloneBigPetal()
+
