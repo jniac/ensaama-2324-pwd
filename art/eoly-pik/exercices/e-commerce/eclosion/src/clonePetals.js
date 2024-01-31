aa3972143e951747985d02b3e3e424f753b41ef0
eoly-pik <eolyna.dumont@ensaama.net>
2024-01-31 11:12:19 +0100

    lololol

--- art/eoly-pik/exercices/e-commerce/eclosion/src/clonePetals.js
+++ art/eoly-pik/exercices/e-commerce/eclosion/src/clonePetals.js
@@ -17,3 +17,26 @@ export function clonePetalB() {
   }
 }
 
+export function clonePetalC() {
+
+  const petal = document.querySelector('.eol .petal-c');
+  for (let i = 1; i < 4; i++) {
+    const clone = petal.cloneNode(true);
+    clone.style.setProperty('--angle', `${(360 / 4) * i}deg`);
+    petal.parentElement.append(clone);
+  }
+}
+
+export function clonePetalD() {
+
+  const petal = document.querySelector('.eol .petal-d');
+  for (let i = 1; i < 4; i++) {
+    const clone = petal.cloneNode(true);
+    clone.style.setProperty('--angle', `${(360 / 4) * i}deg`);
+    petal.parentElement.append(clone);
+  }
+}
+
+
+
+
