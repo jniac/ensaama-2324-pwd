e0b0208d3efc5c965fd7e3a539f23bb89819232b
RSelaries <raphael.selaries@ensaama.net>
2024-02-28 12:15:25 +0100

    push rapide

--- art/RSelaries/eclosion/clonePetalA.js
+++ art/RSelaries/eclosion/clonePetalA.js
@@ -8,3 +8,24 @@ export function clonePetalA() {
         petal.parentElement.append(clone);
     }
 }
+
+export function clonePetalB() {
+    const flower = document.querySelector('.flower')
+    for (let i = 0; i < 6; i++) {
+            const clone = flower.cloneNode(true);
+            clone.classList.remove(`flower`)
+            clone.classList.add(`flower-${i}`, `flowers`)
+            clone.style.setProperty("--angle", ` ${60 * i}deg `);
+            document.querySelector(".bud").append(clone)
+        }
+}
+
+export function clonePetalC() {
+    const petalBig = document.querySelector('.big-petal-0')
+    for (let i = 0; i < 3; i++) {
+        const clone = petalBig.cloneNode(true);
+        clone.classList.remove('big-petal-0')
+        clone.classList.add(`big-petal-${i+1}`)
+        document.querySelector(".bud").append(clone)
+    }
+}
--- art/RSelaries/eclosion/main.js
+++ art/RSelaries/eclosion/main.js
@@ -1,28 +1,39 @@
-import { initEclosion } from "../../../common-projects/eclosion/src/eclosion.js";
-import { replaceByExternalRef, svgFactory } from "../../../common-projects/eclosion/src/tools.js";
-import { clamp, inverseLerp } from "../../../common-resources/js/math-utils.js";
-import { clonePetalA } from './clonePetalA.js'
+import { initEclosion } from "../../../common-projects/eclosion/src/eclosion.js"
+import {
+    replaceByExternalRef,
+    svgFactory,
+} from "../../../common-projects/eclosion/src/tools.js"
+import { clamp, inverseLerp } from "../../../common-resources/js/math-utils.js"
+import { clonePetalA, clonePetalB, clonePetalC } from "./clonePetalA.js"
 
-/** @type {HTMLElement} */
-const root = document.querySelector('.rse')
+function main() {
+    /** @type {HTMLElement} */
+    const root = document.querySelector(".rse")
 
-replaceByExternalRef();
-initEclosion("rse", scroll => {
-  root.style.setProperty('--scroll-1', inverseLerp(0.0, 0.5, scroll).toFixed(3))
-  root.style.setProperty('--scroll-2', inverseLerp(0.485, 1, scroll).toFixed(3))
-  root.style.setProperty('--scroll-big', (inverseLerp(0.5, 1, scroll) * 2).toFixed(3))
-})
+    replaceByExternalRef()
+    initEclosion("rse", (scroll) => {
+        root.style.setProperty( "--scroll-1", inverseLerp(0.0, 0.5, scroll).toFixed(3) )
+        root.style.setProperty( "--scroll-2", inverseLerp(0.485, 1, scroll).toFixed(3) )
+        root.style.setProperty( "--scroll-big", (inverseLerp(0.3, 1, scroll) * 2).toFixed(3) )
+        root.style.setProperty( "--scroll-3", (inverseLerp(0.7, 1, scroll)).toFixed(3) )
+        root.style.setProperty( "--scroll-4", (inverseLerp(0.9, 1, scroll)).toFixed(3) )
+    })
 
+    const d = svgFactory.path.polygon(6, 120, { offsetTurn: 1 / 12 })
 
-const d = svgFactory.path.polygon(6, 120, { offsetTurn: 1 / 12 })
+    const hexagone = () => svgFactory.createNode(`<path d="${d}"/>`)
 
-const hexagone = svgFactory.createNode(`<path d="${d}"/>`)
-document.querySelector('.core svg').append(hexagone)
+    document.querySelector(".core svg").append(hexagone())
+    document.querySelector(".petals svg").append(hexagone())
+    // document.querySelector(".petal-4 svg").append(hexagone())
+    document.querySelector(".big-petal-0 svg").append(hexagone())
 
-const hexagone1 = svgFactory.createNode(`<path d="${d}"/>`)
-document.querySelector('.petals svg').append(hexagone1)
+    clonePetalA()
+    clonePetalB()
+    clonePetalC()
 
-const hexagoneBig = svgFactory.createNode(`<path d="${d}"/>`)
-document.querySelector('.big-petal svg').append(hexagoneBig)
+}
 
-clonePetalA()
+main()
+
+export { main }
