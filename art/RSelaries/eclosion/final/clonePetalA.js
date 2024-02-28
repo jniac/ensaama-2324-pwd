a3392f7d7f1a097686fa59e5a2bfabba595815b6
RSelaries <raphael.selaries@ensaama.net>
2024-02-28 21:01:56 +0100

    Ecosion trop stylé

--- art/RSelaries/eclosion/final/clonePetalA.js
+++ art/RSelaries/eclosion/final/clonePetalA.js
@@ -22,11 +22,93 @@ export function clonePetalB() {
 
 export function clonePetalC() {
     const petalBig = document.querySelector('.big-petal-0')
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 2; i++) {
         const clone = petalBig.cloneNode(true)
         clone.classList.remove('big-petal-0')
         clone.classList.add(`big-petal-${i+1}`)
-        clone.style.setProperty("--rotate-big", `${7.5 * (i+1)}deg`)
+        clone.style.setProperty("--rotate-big", `${20 * (i+1)}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+}
+
+export function cloneTriangle() {
+    const triangle0 = document.querySelector('.triangle-0')
+    for (let i = 0; i < 5; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.add(`triangle-${i+1}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+}
+
+export function cloneTriangle1() {
+    const triangle0 = document.querySelector('.triangle-0')
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add('triangles1')
+        clone.classList.add(`triangle-${i+1}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 20}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add(`triangles1`)
+        clone.classList.add(`triangle-${i+6}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 40}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+}
+
+export function cloneTriangle2() {
+    const triangle0 = document.querySelector('.triangle-0')
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add('triangles2')
+        clone.classList.add(`triangle2-${i}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add(`triangles3`)
+        clone.classList.add(`triangle-${i+6}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add(`triangles4`)
+        clone.classList.add(`triangle-${i+6}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add(`triangles5`)
+        clone.classList.add(`triangle-${i+6}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
+        document.querySelector(".bud").append(clone)
+    }
+    for (let i = 0; i < 6; i++) {
+        const clone = triangle0.cloneNode(true)
+        clone.classList.remove('triangle-0')
+        clone.classList.remove('triangles')
+        clone.classList.add(`triangles6`)
+        clone.classList.add(`triangle-${i+6}`)
+        clone.style.setProperty("--angle", `${(i * 60) + 60}deg`)
         document.querySelector(".bud").append(clone)
     }
 }
--- art/RSelaries/eclosion/final/main.js
+++ art/RSelaries/eclosion/final/main.js
@@ -3,8 +3,11 @@ import {
     replaceByExternalRef,
     svgFactory,
 } from "../../../../common-projects/eclosion/src/tools.js"
-import { clamp, inverseLerp } from "../../../../common-resources/js/math-utils.js"
-import { clonePetalA, clonePetalB, clonePetalC } from "./clonePetalA.js"
+import {
+    clamp,
+    inverseLerp,
+} from "../../../../common-resources/js/math-utils.js"
+import { clonePetalA, clonePetalB, clonePetalC, cloneTriangle, cloneTriangle1, cloneTriangle2 } from "./clonePetalA.js"
 
 function main() {
     /** @type {HTMLElement} */
@@ -12,26 +15,54 @@ function main() {
 
     replaceByExternalRef()
     initEclosion("rse", (scroll) => {
-        root.style.setProperty( "--scroll-1", inverseLerp(0.0, 0.5, scroll).toFixed(3) )
-        root.style.setProperty( "--scroll-2", inverseLerp(0.485, 1, scroll).toFixed(3) )
-        root.style.setProperty( "--scroll-big", (inverseLerp(0, 1, scroll) * 2).toFixed(3) )
-        root.style.setProperty( "--scroll-3", (inverseLerp(0.7, 1, scroll)).toFixed(3) )
-        root.style.setProperty( "--scroll-4", (inverseLerp(0.9, 1, scroll)).toFixed(3) )
+        root.style.setProperty(
+            "--scroll-1",
+            inverseLerp(0.0, 0.5, scroll).toFixed(3)
+        )
+        root.style.setProperty(
+            "--scroll-2",
+            inverseLerp(0.485, 1, scroll).toFixed(3)
+        )
+        root.style.setProperty(
+            "--scroll-big",
+            (inverseLerp(0, 1, scroll) * 2).toFixed(3)
+        )
+        root.style.setProperty(
+            "--scroll-3",
+            inverseLerp(0.7, 1, scroll).toFixed(3)
+        )
+        root.style.setProperty(
+            "--scroll-4",
+            inverseLerp(0.9, 1, scroll).toFixed(3)
+        )
+        root.style.setProperty(
+            "--scroll-5",
+            inverseLerp(0.85, 1, scroll).toFixed(3)
+        )
     })
 
     const d = svgFactory.path.polygon(6, 120, { offsetTurn: 1 / 12 })
+    const tri = svgFactory.path.polygon(3, 60)
 
     const hexagone = () => svgFactory.createNode(`<path d="${d}"/>`)
+    const triangleConstructor = () => svgFactory.createNode(`<path d="${tri}" />`)
+    // const svg = () => document.createElement("svg", { viewBox:"-400 -400 800 800" })
+    // const triangle = () => document.createElement("svg").appendChild(triangleConstructor())
+    const triangle = () => document.createElementNS("http://www.w3.org/2000/svg", "svg").appendChild(triangleConstructor())
 
     document.querySelector(".core svg").append(hexagone())
     document.querySelector(".petals svg").append(hexagone())
-    // document.querySelector(".petal-4 svg").append(hexagone())
     document.querySelector(".big-petal-0 svg").append(hexagone())
+
+    document.querySelector(".big-petal-0 svg").append(hexagone())
+    document.querySelector(".triangle-0 svg").append(triangleConstructor())
 
     clonePetalA()
     clonePetalB()
     clonePetalC()
-
+    cloneTriangle()
+    cloneTriangle1()
+    cloneTriangle2()
 }
 
 main()
