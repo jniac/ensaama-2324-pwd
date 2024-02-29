bc24b246260c4152d55539c10f61c56ec86d2385
jniac <joseph.merdrignac@gmail.com>
2024-02-29 10:06:08 +0100

    rename jniac/eclosion

--- art/jniac/eclosion/final/src/clonePetals.js
+++ art/jniac/eclosion/final/src/clonePetals.js
@@ -1,12 +1,27 @@
 
 export function clonePetalA() {
-  const count = 8
-  const petal = document.querySelector('.jnc .petal-a')
-  for (let i = 1; i < count; i++) {
+  const petal = document.querySelector('.jnc-final-2 .petal-a')
+  for (let i = 1; i < 6; i++) {
     const clone = petal.cloneNode(true)
-    const angle = `${(360 / count) * i}deg`
-    clone.style.setProperty('--angle', angle)
+    clone.style.setProperty('--angle', `${60 * i}deg`)
     petal.parentElement.append(clone)
   }
 }
 
+export function clonePetalB() {
+  const petal = document.querySelector('.jnc-final-2 .petal-b')
+  for (let i = 1; i < 12; i++) {
+    const clone = petal.cloneNode(true)
+    clone.style.setProperty('--angle', `${30 * i}deg`)
+    petal.parentElement.append(clone)
+  }
+}
+
+export function clonePetalC() {
+  const petal = document.querySelector('.jnc-final-2 .petal-c')
+  for (let i = 1; i < 3; i++) {
+    const clone = petal.cloneNode(true)
+    clone.style.setProperty('--angle', `${120 * i}deg`)
+    petal.parentElement.append(clone)
+  }
+}
